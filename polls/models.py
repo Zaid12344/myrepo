ee4284af2a5cc3d8a0f95c55853d921cdec1bbf1
Zaid12344 <shaikhzf2979@gmail.com>
2026-02-12 13:44:25 +0530

    commit

--- polls/models.py
+++ polls/models.py
@@ -10,3 +10,38 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+from django.forms import ModelForm
+
+TITLE_CHOICES = {
+    "MR": "Mr.",
+    "MRS": "Mrs.",
+    "MS": "Ms.",
+}
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=3, choices=TITLE_CHOICES)
+    birth_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+
+
+class AuthorForm(ModelForm):
+    class Meta:
+        model = Author
+        fields = ["name", "title", "birth_date"]
+
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = ["name", "authors"]
